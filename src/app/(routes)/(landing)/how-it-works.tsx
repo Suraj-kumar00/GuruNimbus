df909ccf69e2cc39e8d52e4e289eab6d72b44d8b
Suraj <suraj.ukumar.p@gmail.com>
2024-08-28 12:56:12 +0530

    feat: The 'How it works' video is added.

--- src/app/(routes)/(landing)/how-it-works.tsx
+++ src/app/(routes)/(landing)/how-it-works.tsx
@@ -24,17 +24,7 @@ export function Bttn() {
              
 
               <div className="relative p-2 bg-black rounded-xl">
-                <iframe
-                  width="560"
-                  height="315"
-                  src="https://www.youtube.com/embed/XctooiH0moI?si=prOhlPi84hF-aSbu"
-                  title="YouTube video player"
-                  frameBorder="0"
-                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                  referrerPolicy="strict-origin-when-cross-origin"
-                  allowFullScreen
-                  className="rounded-lg"
-                ></iframe>
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/BREVPg7HK1A?si=bd66DQ7XpTStjTeX" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
               </div>
             </div>
           </ModalContent>
