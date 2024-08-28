6410b0711930ca1ce27df1c511662654e929eae0
Suraj <suraj.ukumar.p@gmail.com>
2024-08-28 13:04:27 +0530

    feat: The how-it-works page is modified.

--- src/app/(routes)/(landing)/how-it-works.tsx
+++ src/app/(routes)/(landing)/how-it-works.tsx
@@ -20,11 +20,14 @@ export function Bttn() {
         <ModalBody>
           <ModalContent>
             <div className="relative flex justify-center items-center">
-
-             
-
               <div className="relative p-2 bg-black rounded-xl">
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/BREVPg7HK1A?si=bd66DQ7XpTStjTeX" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+              <iframe width="560" height="315" 
+              src="https://www.youtube.com/embed/BREVPg7HK1A?si=bd66DQ7XpTStjTeX" 
+              title="YouTube video player"  
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+              referrerPolicy="strict-origin-when-cross-origin" 
+              allowFullScreen>
+              </iframe>
               </div>
             </div>
           </ModalContent>
